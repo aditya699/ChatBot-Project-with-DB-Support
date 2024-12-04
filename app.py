22a7c47f2268f92d299c72728c727c54a6440c27
aditya <meenakshi.bhtt@gmail.com>
2024-12-04 20:18:19 +0530

    added chainlit boiler plate

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+import chainlit as cl
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+
+# Load environment variables from .env file
+load_dotenv()
+ANTHROPIC_API_KEY=os.getenv("ANTHROPIC_API_KEY")
+
+llm = ChatAnthropic(
+    model="claude-3-haiku-20240307",
+    temperature=0,
+    max_tokens=4092,
+    timeout=None,
+    max_retries=2,
+    api_key=ANTHROPIC_API_KEY
+    # other params...
+)
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="General Company Policies",
+            message="Can help with leave Policy",
+            icon="static/images/policy.svg",
+            ),
+
+        cl.Starter(
+            label="General Tickets Information",
+            message="Can help you with all Customer Related Information",
+            icon="static/images/tickets.PNG",
+            ),
+        cl.Starter(
+            label="Can help me general Query",
+            message="What is AI?",
+            icon="static/images/query.PNG",
+            )
+        ]
+
+@cl.on_chat_start
+def on_chat_start():
+    print("A new chat session has started!")
+
+@cl.on_message
+async def main(message: cl.Message):
+
+    user=message.content
+
+    response="The Bot is under Development"
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {response}",
+    ).send()
